76e4572aaa9c80a25ee8dca1fb795056991d34e1
Vinicius Salvador <79427784+ViniciusSalvador11@users.noreply.github.com>
2022-10-11 11:30:55 -0300

    Add files via upload

---
+++ subject.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaceDesignPatterns;
+
+/**
+ *
+ * @author 821150867
+ */
+public interface subject {
+    
+    public void registerObserver();
+}
